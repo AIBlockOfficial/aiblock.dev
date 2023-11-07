e0486f651dc23e17889efbcb9e9ec5f6df40e192
BHouwens <zhion360@gmail.com>
2023-11-07 14:23:10 +0100

    Adding basic payment tutorials

--- docs/basic-tutorials/sidebar.js
+++ docs/basic-tutorials/sidebar.js
@@ -1,8 +1,19 @@
 module.exports = [{
     "type": "doc",
-    "id": "basic-tutorials",
-}, {
-    "type": "doc",
     "label": "get-started",
     "id": "basic-tutorials/get-started"
+},
+{
+    "type": "doc",
+    "label": "send-and-receive",
+    "id": "basic-tutorials/send-and-receive"
+},
+{
+    "type": "doc",
+    "label": "two-way-payments",
+    "id": "basic-tutorials/two-way-payments"
+},{
+    "type": "doc",
+    "label": "create-an-item",
+    "id": "basic-tutorials/create-an-item"
 }];
